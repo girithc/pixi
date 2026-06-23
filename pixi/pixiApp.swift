b70fce000437b4322b437370279ec0336d2e856f
girithc <girithmchoudhary@gmail.com>
2026-06-22 20:03:03 -0700

    Initial Commit

---
+++ pixi/pixiApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixiApp.swift
+//  pixi
+//
+//  Created by Girith Choudhary on 6/22/26.
+//
+
+import SwiftUI
+
+@main
+struct pixiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
